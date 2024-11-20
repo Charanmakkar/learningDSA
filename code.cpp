6fa80a53fdaedfb6fd368c0036e0a24eb3fc6361
charanmakkar <charanmakkar2@gmail.com>
2024-11-20 13:11:36 +0530

    push

--- code.cpp
+++ code.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main(){
-    cout << "Charanpreet Singh\n";
+    // cout << "Charanpreet Singh\n\n";
+
+
+
+    // for(int a=0; a<20; a++){
+    //     cout << a++ << endl;
+    // }
+
+
+
+    // char input;
+    // while(true){
+    
+    //     cout << "Enter a Char : ";
+    //     cin >> input;
+
+    //     // cout << input << endl;
+
+    //     if (input >= 'A' && input <= 'Z'){
+    //         cout << "Upper Case\n\n";
+    //     }
+    //     if (input>='a' && input<='z'){
+    //         cout << "Lower Case\n\n";
+    //     }
+    //     else{
+    //         cout << "Not a Alphabet\n";
+    //     }
+
+    //     input = 0;
+    // }
+
+
+    // char input;
+    // while(true){    
+    //     cout << "Enter a Char : ";
+    //     cin >> input;
+
+    //     // cout << input << endl;
+
+    //     if (input >= 65 && input <= 90){
+    //         cout << "Upper Case\n\n";
+    //     }
+    //     if (input>=97 && input<=122){
+    //         cout << "Lower Case\n\n";
+    //     }
+    //     else{
+    //         cout << "Not a Alphabet\n";
+    //     }
+
+    //     input = 0;
+    // }
+
+
+    
+
+
     return 0;
 }
